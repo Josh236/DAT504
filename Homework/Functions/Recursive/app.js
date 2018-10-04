935d647c4d5571223c733d38ff12b83b6e7583ac
Josh236 <holdaway236@gmail.com>
2018-10-04 12:01:56 +0100

    hmw

---
+++ Homework/Functions/Recursive/app.js
@@ -0,0 +1,8 @@
+function isEven(number) {
+  if (number == 0) return true;
+  else if (number == 1) return false;
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
---
+++ Homework/Program Structure/Chessboard/app.js
@@ -0,0 +1,12 @@
+let black = "#";
+let white = " ";
+let size = 8;
+let board = "";
+
+for (let y = 0; y < size; y++){
+  for (let x = 0; x < size; x++){
+    
+  }
+}
+
+console.log(board);
