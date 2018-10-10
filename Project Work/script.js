73c0a0d44797e068942f1a0c2b5f7f22d598ff13
Josh236 <holdaway236@gmail.com>
2018-10-10 11:57:09 +0100

    first

--- Project Work/script.js
+++ Project Work/script.js
@@ -1,7 +1,7 @@
 var request = require("request");
 
 var options = { method: 'GET',
-  url: 'https://api.themoviedb.org/3/movie/top_rated',
+  url: 'https://api.themoviedb.org/3/search/movie?api_key=83b6a07dc9c37585418713789d05054b&language=en-US&query=John&page=1&include_adult=false',
   qs:
    { page: '1',
      language: 'en-US',
